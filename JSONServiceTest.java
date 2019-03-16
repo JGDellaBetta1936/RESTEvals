db913117121e5a619395c782d5b58ababf20768d
CrunchifyRESTJerseyExample02152017 <john_gold@comcast.net>
2019-03-16 12:41:58 -0400

    Add files via upload

---
+++ JSONServiceTest.java
@@ -0,0 +1,19 @@
+package com.mkyong.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JSONServiceTest {
+
+	@Test
+	public void testGetProductInJSON() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateProductInJSON() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ProductTest.java
@@ -0,0 +1,34 @@
+package com.mkyong.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProductTest {
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetQty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetQty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
